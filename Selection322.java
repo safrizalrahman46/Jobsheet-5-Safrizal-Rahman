2a113ef4a6686f38c725e6766efb81fdebbbbe86
KazeKagi <safrizalrahman46@gmail.com>
2023-10-22 21:28:55 +0700

    Push 3 + Clear First Break To Github SAFRIZAL RAHMAN

--- Selection322.java
+++ Selection322.java
@@ -16,7 +16,7 @@ public class Selection322 {
         switch(operator){ 
             case '+': result = operand1 + operand2; 
         System.out.println(operand1+" + "+operand2+" = "+result); 
-        break; 
+        
             case '-': result = operand1 - operand2; 
         System.out.println(operand1+" - "+operand2+" = "+result); 
         break; 
