37006513dbf980eeeb62fc558a88530a59e9c328
KazeKagi <safrizalrahman46@gmail.com>
2023-10-22 21:05:21 +0700

    Push 2 + Letter Grade (SCORE) To Github SAFRIZAL RAHMAN

--- Selection222.java
+++ Selection222.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Selection222 {
     public static void main(String[] args) {
         Scanner inputan22 = new Scanner(System.in);
-        
+        String Score;
+
         System.out.println("Score UAS : ");
         float finalExam = inputan22.nextFloat();
         System.out.println("Score UTS : ");
@@ -17,5 +18,23 @@ public class Selection222 {
         float total = (finalExam*0.4F)+(midExam*0.3F)+(quiz*0.2F)+(assignment*0.1F);
         String message = total < 65 ? "Retake ":"Pass"; 
         System.out.println("Final Grade = "+total+" and the decission is  "+message);
+
+        if (total <= 39) {
+            Score = "E"; 
+            System.out.println("You Are on Grade  "+Score); 
+            }else if(total <= 50){ Score = "D"; 
+            System.out.println("You Are on Grade  "+Score); 
+            }else if(total<=60){ Score = "C"; 
+            System.out.println("You Are on Grade  "+Score); 
+            }else if(total<=65){ Score = "C+"; 
+            System.out.println("You Are on Grade  "+Score); 
+            }else if(total<=73){ Score = "B"; 
+            System.out.println("You Are on Grade  "+Score); 
+            }else if(total<=80){ Score = "B+"; 
+            System.out.println("You Are on Grade  "+Score);
+            }else if(total<=100){ Score = "A"; 
+            System.out.println("You Are on Grade  "+Score); 
+            
+        }
     }
 }
